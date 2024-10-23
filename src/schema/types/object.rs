1d73758c45540de8b804493a8728755c3e0d043b
Houtamelo <antoniopedrogf@hotmail.com>
2024-10-23 15:24:31 -0300

    Moved main docs to README.md

--- src/schema/types/object.rs
+++ src/schema/types/object.rs
@@ -1,5 +1,5 @@
-use crate::schema::shared_impls::all_shared_impls;
 use super::*;
+use crate::schema::shared_impls::all_shared_impls;
 
 #[derive(Clone, Debug, Default)]
 pub struct JObject {
@@ -12,11 +12,11 @@ impl JObject {
 	pub fn new() -> Self {
 		Self::default()
 	}
-	
+
 	pub fn add_property(&mut self, key: impl Into<String>, ty: impl Into<Type>) {
 		self.properties.insert(key.into(), ty.into());
 	}
-	
+
 	pub fn with_properties(properties: impl Iterator<Item = (impl Into<String>, impl Into<Type>)>) -> Self {
 		Self {
 			description: None,
@@ -25,7 +25,7 @@ impl JObject {
 				.collect(),
 		}
 	}
-	
+
 	pub fn insert_variant_definitions(&self, fill_me: &mut Vec<VariantDefinition>) {
 		for ty in self.properties.values() {
 			ty.insert_variant_definitions(fill_me);
@@ -36,13 +36,13 @@ impl JObject {
 impl SerializeFields for JObject {
 	fn serialize_fields<M: SerializeMap>(&self, map: &mut M) -> Result<(), M::Error> {
 		map.serialize_entry("type", "object")?;
-		
+
 		if !self.properties.is_empty() {
 			map.serialize_entry("properties", &self.properties)?;
 			map.serialize_entry("required", &self.properties.keys().collect::<Vec<_>>())?;
 			map.serialize_entry("additionalProperties", &false)?;
-		} 
-		
+		}
+
 		Ok(())
 	}
 }
